ea2fbccd667f707711c2b060613897b99dbcf987
souravp123 <sourav.p@yekasolutions.com>
2025-12-11 17:14:47 +0530

    error change one

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from './pages/Home';
 function App() {
   return (
     <>
-      <BrowserRouter basename="/galtech">
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path='/' element={<Home/>}/>
         </Routes>
